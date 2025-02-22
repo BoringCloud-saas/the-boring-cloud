f4cf4b536f545658faaf1f3de4f758816cad57c7
zdravkoCrypto <zdravko.jovanovic.tech@gmail.com>
2025-02-22 15:35:29 +0100

    name changes

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -20,7 +20,7 @@ export default function Navigation() {
     return (
         <div className="flex justify-between items-center rounded-md bg-[#fafafa]">
             <div className="flex items-center ml-[10%]">
-                <h1 className="text-2xl font-semibold text-[#000000 mr-1">SaaS</h1>
+                <h1 className="text-2xl font-semibold text-[#000000 mr-1">Logic Mail</h1>
             </div>
 
             <div className="flex p-2 items-center mr-[10%]">
--- app/home/components/Navigation.tsx
+++ app/home/components/Navigation.tsx
@@ -8,7 +8,7 @@ export default function Navigation({ username }: NavigationProps) {
   return (
       <div className="flex justify-between items-center p-2 rounded-md bg-[#fafafa]">
           <div className="flex items-center ml-[10%]">
-              <h1 className="text-2xl font-semibold text-[#000000] mr-1">SaaS</h1>
+              <h1 className="text-2xl font-semibold text-[#000000] mr-1">Logic Mail</h1>
           </div>
 
           <div className="flex p-2 items-center mr-[10%]">
